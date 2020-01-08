b3c6c2f2c179ef37da5fc5606ebb1d754ee8eaac
Kevin_Lin <zexion7873@gmail.com>
2020-01-08 20:11:32 +0800

    手動 Commit Offset

--- src/main/java/com/thinkpower/springcloudstreamkafka/Service/Consumer.java
+++ src/main/java/com/thinkpower/springcloudstreamkafka/Service/Consumer.java
@@ -7,6 +7,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.SendTo;
 
@@ -19,18 +21,12 @@ public class Consumer {
     @SendTo({Reply.OUTPUT})
     public BookDTO consume(Message<BookDTO> message) {
 
-//        Acknowledgment acknowledgment = message.getHeaders()
-//                .get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
-//
-//        System.err.println("Acknowledgment : " + acknowledgment);
-//        if (acknowledgment != null) {
-//            System.out.println("Acknowledgment provided");
-//            acknowledgment.acknowledge();
-//        }
         logger.info("myProject Received a message : {}", message);
         logger.info("uuid : {}", message.getHeaders().getOrDefault("uuid", null));
         logger.info("partition : {}", message.getHeaders().getOrDefault("kafka_receivedPartitionId", null));
         logger.info("offset : {}", message.getHeaders().getOrDefault("kafka_offset", null));
+
+        offsetCommit(message);
 
         return message.getPayload();
     }
@@ -43,6 +39,20 @@ public class Consumer {
     @StreamListener(Reply.INPUT)
     public void reply(BookDTO reply) {
 //        logger.info("Reply Book : {}", reply.getName());
+    }
+
+    /**
+     * 手動 Commit Offset
+     * @param message 訊息
+     */
+    private void offsetCommit(Message<?> message) {
+        Acknowledgment acknowledgment = message.getHeaders()
+                .get(KafkaHeaders.ACKNOWLEDGMENT, Acknowledgment.class);
+        System.err.println("Acknowledgment : " + acknowledgment);
+        if (acknowledgment != null) {
+            System.out.println("Acknowledgment provided");
+            acknowledgment.acknowledge();
+        }
     }
 
 
